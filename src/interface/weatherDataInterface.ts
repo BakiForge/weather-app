066fd0cf3499ad69672b11c9cbf3ad01dd61e99f
BakiForge <bakiforge@gmail.com>
2026-07-29 17:27:36 +0200

    * Create interface for weatherData

---
+++ src/interface/weatherDataInterface.ts
@@ -0,0 +1,10 @@
+export interface weatherData {
+    name: string;
+    description: string,
+        temp: number;
+        feels_like: number;
+        humidity: number;
+        pressure: number;
+        windSpeed: number;
+    visibility: number;
+}
